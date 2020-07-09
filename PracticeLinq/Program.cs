66dee502276e755655936d26c211320a73c7ea46
unknown <jayapatel864@gmail.com>
2020-07-09 13:47:47 -0500

    finished 7 9

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carNames = { "Toyota","Dodge", "Ford", "Mazda"};
+            var letterSelection = from x in carNames
+                                  where x.Contains ("o")
+                                  orderby x descending
+                                  select x;
+
+                                  
+            foreach (var i in letterSelection)
+            {
+                return ;
+
+            }
+
+        }
+    }
+}
